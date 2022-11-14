d246ca0b38e8a67f32ace8b8bca5a0110b8748f6
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2022-11-14 21:09:13 +0100

    Revert "Download plans in 5 minute intervals"
    This reverts commit 1587dbf0000ab3aa2e9676fa198f36c86fe144d2.

--- src/main/java/org/pipeman/sp_api/API.java
+++ src/main/java/org/pipeman/sp_api/API.java
@@ -3,11 +3,13 @@ package org.pipeman.sp_api;
 import io.javalin.http.Context;
 
 public class API {
+    private static final PlanDownloader DOWNLOADER = new PlanDownloader(Main.conf());
+
     public static void getPlanToday(Context ctx) {
-        ctx.html(PlanDownloader.INSTANCE.getTodayPlan());
+        ctx.html(DOWNLOADER.getTodayPlan());
     }
 
     public static void getPlanTomorrow(Context ctx) {
-        ctx.html(PlanDownloader.INSTANCE.getTomorrowPlan());
+        ctx.html(DOWNLOADER.getTomorrowPlan());
     }
 }

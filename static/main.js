959eb20cd972562177aa17e48de08b2d28744b59
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2023-01-03 10:33:28 +0100

    Fix settings not being persistent

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 let todayReady = false;
 let tomorrowReady = false;
-const tables = showTables();
+let tables;
+let lightMode;
 const moon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="var(--text-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class=""><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>';
 const sun = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="var(--text-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class=""><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
 
@@ -8,33 +9,33 @@ const delay = setTimeout(() => {
     setLoading(true);
 }, 100);
 
+function loadSettings() {
+    const settings = parseInt(localStorage.getItem("settings"));
+    if (isNaN(settings)) {
+        lightMode = true;
+        tables = false;
+    } else {
+        lightMode = Boolean(settings & 1);
+        tables = Boolean(settings & 2);
+    }
+    saveSettings();
+}
+
+function saveSettings() {
+    localStorage.setItem("settings", ((lightMode ? 1 : 0) | (tables ? 2 : 0)).toString());
+}
+
 function isBlank(string) {
     return string == null || string.trim() === '';
 }
 
 function toggleTheme() {
-    const mode = !isLightMode();
-    localStorage.setItem("theme", mode ? "default" : "dark");
-    setTheme(mode);
+    lightMode = !lightMode;
+    setTheme(lightMode);
+    saveSettings();
 }
 
-function isLightMode() {
-    if (localStorage.getItem("theme") === "dark") return false;
-    else {
-        localStorage.setItem("theme", "default");
-        return true;
-    }
-}
-
-function showTables() {
-    if (localStorage.getItem("table") === "true") return true;
-    else {
-        localStorage.setItem("theme", "false");
-        return false;
-    }
-}
-
-function setTheme(lightMode) {
+function setTheme() {
     document.getElementById("theme-css").href = lightMode ? "default.css" : "dark.css";
     document.getElementById("theme-button").innerHTML = lightMode ? moon : sun;
 }
@@ -51,7 +52,7 @@ function onLoadEnd() {
     }
 }
 
-function getPlans(tables) {
+function getPlans() {
     if (tables) {
         fetch("api/plans/today?format=json").then(r => r.json()
             .then(j => {
@@ -138,7 +139,8 @@ function addTableRow(table, clazz, lesson, substitution, teacher, room, other) {
 }
 
 function toggleTable() {
-    localStorage.setItem("table", (!tables).toString());
+    tables = !tables;
+    saveSettings();
     location.reload();
 }
 
@@ -164,5 +166,6 @@ window.addEventListener('resize', () => {
     onResize();
 });
 
-setTheme(isLightMode());
-getPlans(tables);
+loadSettings();
+setTheme();
+getPlans();
